f07089197adc5501253d560fbc5c21e62e100470
shreyas <shreyas.waykar123@gmail.com>
2022-08-22 16:24:33 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Typed from "react-typed";
 
-function Hero() {}
+function Hero() {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl  font-bold">
+          Grow With Data
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexible financing for
+            <Typed
+              strings={[" BTB", " BTC", " SAAS"]}
+              typeSpeed={120}
+              backSpeed={140}
+              loop
+            />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 font-serif">
+    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 ">
       <h1 className="w-full text-3xl font-bold  text-[#00df9a]">Neo Bank</h1>
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
